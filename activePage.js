d7ee01598b626f6dc7c1b6c4b5481c037eb1b949
Giorgos Melas <105015592+giorgosmelas2@users.noreply.github.com>
2024-11-04 01:25:56 +0200

    Update activePage.js

--- activePage.js
+++ activePage.js
@@ -1,5 +1,3 @@
-/*show the different pages*/ 
-
 function showPage(pageId) {
     var pages = document.getElementsByClassName('content');
     for (var i = 0; i < pages.length; i++) {
@@ -72,13 +70,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
       this.classList.add("active");
 
       if (this.getAttribute("onclick") === "showPage('about')") {
-        animateProgressBar(75, 2000); // Set the desired percentage and duration here for the first progress bar
-        animateProgressBar2(50, 1500); // Set the desired percentage and duration here for the second progress bar
-        animateProgressBar3(80, 2500); // Set the desired percentage and duration here for the third progress bar
-        animateProgressBar4(60, 1200); // Set the desired percentage and duration here for the fourth progress bar
+        animateProgressBar(75, 2000); // Set the desired percentage and duration here for the progress bar
+        animateProgressBar2(50, 1500); 
+        animateProgressBar3(80, 2500); 
+        animateProgressBar4(60, 1200); 
         animateProgressBar5(55, 1600);
         animateProgressBar6(40, 1000);
-        // Add more function calls to animate additional progress bars if needed
+          
       } else {
         // Reset the progress bars
         document.getElementById('progress1').style.width = '0%';
@@ -87,7 +85,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById('progress4').style.width = '0%';
         document.getElementById('progress5').style.width = '0%';
         document.getElementById('progress6').style.width = '0%';
-        // Add more lines to reset other progress bars if needed
       }
     });
   }
@@ -126,7 +123,7 @@ function animateProgressBar2(targetWidth, duration) {
 function animateProgressBar3(targetWidth, duration) {
   var progressBar = document.getElementById('progress3');
   var currentWidth = 0;
-  var increment = targetWidth / (duration / 10); // Divide by 10 to make it smoother
+  var increment = targetWidth / (duration / 10);
 
   var interval = setInterval(function() {
     if (currentWidth >= targetWidth) {
@@ -141,7 +138,7 @@ function animateProgressBar3(targetWidth, duration) {
 function animateProgressBar4(targetWidth, duration) {
   var progressBar = document.getElementById('progress4');
   var currentWidth = 0;
-  var increment = targetWidth / (duration / 10); // Divide by 10 to make it smoother
+  var increment = targetWidth / (duration / 10); 
 
   var interval = setInterval(function() {
     if (currentWidth >= targetWidth) {
@@ -156,7 +153,7 @@ function animateProgressBar4(targetWidth, duration) {
 function animateProgressBar5(targetWidth, duration) {
   var progressBar = document.getElementById('progress5');
   var currentWidth = 0;
-  var increment = targetWidth / (duration / 10); // Divide by 10 to make it smoother
+  var increment = targetWidth / (duration / 10); 
 
   var interval = setInterval(function() {
     if (currentWidth >= targetWidth) {
@@ -171,7 +168,7 @@ function animateProgressBar5(targetWidth, duration) {
 function animateProgressBar6(targetWidth, duration) {
   var progressBar = document.getElementById('progress6');
   var currentWidth = 0;
-  var increment = targetWidth / (duration / 10); // Divide by 10 to make it smoother
+  var increment = targetWidth / (duration / 10);
 
   var interval = setInterval(function() {
     if (currentWidth >= targetWidth) {
